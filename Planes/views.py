bc00ce308d06af29da20f2149d984a7611bcd06d
Jinxer <pro.nithin.raj@gmail.com>
2022-10-16 18:56:35 +0530

    Html Rendered

---
+++ Planes/views.py
@@ -0,0 +1,8 @@
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request,'index.html')
